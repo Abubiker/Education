6d40112a0374e65059cffe97472f8b693f78ec5e
Abubiker <Abubiker@>
2023-07-09 20:36:27 +0300

    HomeWork4_2 math.random

---
+++ src/Lesson4/HomeWork4_2.java
@@ -0,0 +1,33 @@
+package Lesson4;
+
+
+import java.util.Scanner;
+
+public class HomeWork4_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+        int[] numbers = new int[size];
+        for (int i = 0; i < size; i++) {
+            numbers[i] = (int) (Math.random() * 100); // Генерируем случайное число от 0 до 99
+        }
+        int max = numbers[0];
+        int min = numbers[0];
+        int sum = 0;
+        double average;
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+            if (number < min) {
+                min = number;
+            }
+            sum += number;
+        }
+        average = (double) sum / size;
+        System.out.println("Максимальное значение: " + max);
+        System.out.println("Минимальное значение: " + min);
+        System.out.println("Среднее значение: " + average);
+    }
+}
