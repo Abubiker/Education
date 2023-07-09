a11a1da6b6b302a4862ec0858ccc42274387cb8b
Abubiker <Abubiker@>
2023-07-10 00:11:02 +0300

    HomeWork4 wellDone

---
+++ src/Lesson4/HomeWork4_10.java
@@ -0,0 +1,41 @@
+package Lesson4;
+
+public class HomeWork4_10 {
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+                {1, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 0, 0}
+        };
+        int[][] matrix2 = {
+                {1, 2, 3},
+                {1, 1, 1},
+                {0, 0, 0},
+                {2, 1, 0}
+        };
+        int rows1 = matrix1.length;
+        int cols1 = matrix1[0].length;
+        int rows2 = matrix2.length;
+        int cols2 = matrix2[0].length;
+
+        if (cols1 != rows2) {
+            System.out.println("Умножение матриц невозможно");
+            return;
+        }
+        int[][] result = new int[rows1][cols2];
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                for (int k = 0; k < cols1; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        System.out.println("Результат умножения матриц:");
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
