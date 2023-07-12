e7d781ac0cf029ce482e4c65d0887b20b6fb1bd9
Abubiker <Abubiker@>
2023-07-12 21:24:09 +0300

    Credit Card

---
+++ src/Lesson5/CreditCard/CreditCard.java
@@ -0,0 +1,41 @@
+package Lesson5.CreditCard;
+
+public class CreditCard {
+    String number;
+    int balance;
+
+    CreditCard(String number, int initialBalance) {
+        this.number = number;
+        this.balance = initialBalance;
+    }
+
+    void deposit(int amount) {
+        balance += amount;
+    }
+
+    void withdraw(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Мало денег");
+        }
+    }
+    void printCardInfo() {
+        System.out.println("Номер Счета: " + number);
+        System.out.println("Ваш Баланс: " + balance);
+    }
+
+    public static void main(String[] args) {
+        CreditCard card1 = new CreditCard("3553535", 0);
+        CreditCard card2 = new CreditCard("9898989", 0);
+        CreditCard card3 = new CreditCard("4141414", 600);
+
+        card1.deposit(100000);
+        card2.deposit(777);
+        card3.withdraw(300);
+
+        card1.printCardInfo();
+        card2.printCardInfo();
+        card3.printCardInfo();
+    }
+}
